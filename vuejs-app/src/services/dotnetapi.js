4cb5e197ce4c901682b3b3aadbb859b0729e70ed
Denny Triana <me@dennytriana.com>
2024-07-28 15:44:53 +0700

    [VueJS] Adjust config for production

--- vuejs-app/src/services/dotnetapi.js
+++ vuejs-app/src/services/dotnetapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const dotnetApi = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://dotnet.dennytriana.com/api'
 });
 
 export const createApiData = data => {
--- vuejs-app/src/services/symfonyapi.js
+++ vuejs-app/src/services/symfonyapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const symfonyApi = axios.create({
-  baseURL: 'http://localhost:8081/api'
+  baseURL: 'https://symfony.dennytriana.com/api'
 });
 
 export const getApiData = () => {
